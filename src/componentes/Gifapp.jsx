df694f4c27d8faffeaf2b6ee5f477ae3e0365904
JairPrada <JairPrada9@gmail.com>
2021-02-17 14:35:57 -0500

    Modificando Footer

--- src/componentes/Gifapp.jsx
+++ src/componentes/Gifapp.jsx
@@ -1,18 +1,32 @@
-import { Divider, Typography } from 'antd';
-import React, { useState, Fragment } from 'react';
+import { Divider, Typography, BackTop, notification } from 'antd';
+import React, { useState, Fragment, useEffect } from 'react';
 import AgregarCategoria from './AgregarCategoria';
 import GridImagenes from './GridImagenes';
 import { Layout, Breadcrumb } from 'antd';
+import { LinkedinOutlined, WhatsAppOutlined, GithubOutlined, HomeOutlined } from '@ant-design/icons';
 const { Content, Footer } = Layout;
 const { Title } = Typography;
 
 const Gifapp = (props) => {
-    const [categorias, setCategorias] = useState(["Fire"]);
+    const [categorias, setCategorias] = useState(["PSG"]);
+    const [primera, setPrimera] = useState(true)
+    useEffect(() => {
+        if (primera === true) {
+            notification.open({
+                message: 'Bienvenido a Giff App',
+                description:
+                    'Esta aplicacion esta diseñada para que  busques los mejores gifs, animate y prueba ',
+                icon: <HomeOutlined style={{ color: '#108ee9' }} />,
+            });
+            setPrimera(false)
+        }
+    }, [primera])
     return (
         <Fragment>
             <Layout id="contenido">
+                <BackTop />
                 <Content style={{ padding: "50px", minHeight: "900px" }}>
-                    <Title level={1} style={{ color: "white" ,fontSize:"3rem"}} className="entrandoDerecha">Giff App</Title>
+                    <Title level={1} style={{ color: "white", fontSize: "3rem" }} className="entrandoDerecha">Giff App</Title>
                     <AgregarCategoria setCategorias={setCategorias} />
                     <Divider />
                     <Breadcrumb className="entrandoDerecha" style={{ margin: '16px 0', color: "white" }}>
@@ -25,7 +39,17 @@ const Gifapp = (props) => {
                     }
                 </Content>
                 <Footer className="footer" style={{ textAlign: 'center', marginTop: '25vh', backgroundColor: "#13303F", color: "white" }}>
-                    Derechos reservados
+                    Jairprada9@gmail.com
+                    <br />
+                    <a href="http://www.github.com/JairPrada" style={{ fontSize: "20px", margin: "5px", color: "white" }} target="_blank" rel="noopener noreferrer">
+                        <GithubOutlined />
+                    </a>
+                    <a href="https://www.linkedin.com/in/jair-andres-prada-sierra-462aa11ab/" style={{ fontSize: "20px", margin: "5px", color: "white" }} target="_blank" rel="noopener noreferrer">
+                        <LinkedinOutlined style={{ fontSize: "20px", margin: "5px" }} />
+                    </a>
+                    <a href="https://api.whatsapp.com/send?phone=573167692857&text=%20" style={{ fontSize: "20px", margin: "5px", color: "white" }} target="_blank" rel="noopener noreferrer">
+                        <WhatsAppOutlined style={{ fontSize: "20px", margin: "5px" }} />
+                    </a>
                 </Footer>
 
             </Layout>
